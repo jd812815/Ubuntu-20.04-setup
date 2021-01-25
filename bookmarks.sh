76071a8055fba0b46dce58c508fef9193d1e0fbb
jd812815 <77907449+jd812815@users.noreply.github.com>
2021-01-25 12:34:12 -0500

    Create bookmarks.sh

---
+++ bookmarks.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Roundcube:
+https://www.linuxbabe.com/ubuntu/install-roundcube-webmail-ubuntu-20-04-apache-nginx
+
+XMPP:
+https://computingforgeeks.com/install-ejabberd-xmpp-server-on-ubuntu/
+https://www.process-one.net/en/ejabberd/downloads/
