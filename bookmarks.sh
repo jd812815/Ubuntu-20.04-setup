5cdf1f2d7e3e91271b98249637a3a3f69e39f0e6
jd812815 <77907449+jd812815@users.noreply.github.com>
2021-01-25 20:50:37 -0500

    Update bookmarks.sh

--- bookmarks.sh
+++ bookmarks.sh
@@ -6,3 +6,6 @@ https://www.linuxbabe.com/ubuntu/install-roundcube-webmail-ubuntu-20-04-apache-n
 XMPP:
 https://computingforgeeks.com/install-ejabberd-xmpp-server-on-ubuntu/
 https://www.process-one.net/en/ejabberd/downloads/
+
+Email:
+https://www.linuxbabe.com/mail-server/ubuntu-18-04-iredmail-email-server
